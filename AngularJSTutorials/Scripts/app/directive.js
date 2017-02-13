fe448b10294e649b2105dc3884c2463c844f71a1
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-02-13 17:03:08 +0700

    add

---
+++ AngularJSTutorials/Scripts/app/directive.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module("myApp", []);
+
+app.directive("w3TestDirective", function () {
+    return {
+        template: "I was made in a directive constructor!"
+    };
+});
---
+++ AngularJSTutorials/Scripts/app/moduls.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../angular.min.js" />
+var app = angular.module('myApp', []);
+app.controller('myController', function ($scope) {
+    $scope.firstName = 'Thịnh';
+    $scope.lastName = 'Nguyễn Thành';
+});
+app.directive('w3TestDirective', function ($scope) {
+    return {
+        template: "I was made in a directive constructor!"
+    };
+});
